0194dd2a7378d8c9e9696f087c0745336bfbb858
Yash19652 <113798540+Yash19652@users.noreply.github.com>
2024-01-26 20:59:59 +0530

    created signup api route

--- api/index.js
+++ api/index.js
@@ -2,6 +2,8 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRoutes from './routes/user.route.js';
+// exported as router imported as userroutes
+import authRoutes from './routes/auth.route.js';
 
 dotenv.config();
 
@@ -11,9 +13,11 @@ mongoose.connect(process.env.MONGO).then(() => {
     console.log(err);
 });
 const app = express();
+app.use(express.json());
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000!!');
 });
 
 app.use('/api/user', userRoutes);
+app.use('/api/auth', authRoutes);
