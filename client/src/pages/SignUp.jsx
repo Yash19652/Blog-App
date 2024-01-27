e3bfadfcf55830cbe54984f7819fbd6e2e391549
Yash19652 <113798540+Yash19652@users.noreply.github.com>
2024-01-27 17:20:36 +0530

    height adjusted

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -41,7 +41,7 @@ function SignUp() {
   }
   
   return (
-    <div className='min-height-screen mt-20'>
+    <div className='min-h-screen mt-20'>
       <div className='flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-5'>
       {/* left */}
       <div className="flex-1">
